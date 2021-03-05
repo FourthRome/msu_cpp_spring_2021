dd5d5b3a9e5dd77b8f2e4201ee68e07da7ff8fb6
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-03-05 05:08:35 +0300

    Added template files, including Makefile

---
+++ template/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <cassert>
+#include "program.hpp"
+
+
+int main()
+{
+    std::cout << "Starting testing:" << std::endl;
+
+    std::cout << "All tests passed successfully." << std::endl;
+
+    return 0;
+}
